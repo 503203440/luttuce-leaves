1a9550045517eac76a48e48359af2d06b4864dda
YX <503203440@qq.com>
2021-11-04 17:32:43 +0800

    newConnect

--- src/main/java/org/yx/lettuce_leaves/IndexController.java
+++ src/main/java/org/yx/lettuce_leaves/IndexController.java
@@ -1,9 +1,14 @@
 package org.yx.lettuce_leaves;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
@@ -15,10 +20,6 @@ import java.util.Optional;
 @Slf4j
 public class IndexController {
 
-
-    @FXML
-    private Button exit;
-
     @FXML
     public void exit() {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "要退出lettuce-leaves吗", ButtonType.YES, ButtonType.NO);
@@ -29,4 +30,24 @@ public class IndexController {
         }
     }
 
+    @FXML
+    public void newConnect(ActionEvent event) {
+        String name = event.getSource().getClass().getName();
+        Object eventSource = event.getSource();
+        if (eventSource instanceof MenuItem) {
+            MenuItem menuItem = (MenuItem) eventSource;
+            Window ownerWindow = menuItem.getParentPopup().getOwnerWindow();
+            System.out.println(name);
+            Stage stage = new Stage();
+            stage.setTitle("新建连接");
+            stage.initOwner(ownerWindow);
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.show();
+        }
+    }
+
+    @FXML
+    public void about(ActionEvent event) {
+
+    }
 }
